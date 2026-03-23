a357dbd09a884a68497dec50f7eb10ac98ddb34d
Nightmaresilent <yurianticonishi@gmail.com>
2026-03-23 02:53:25 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,7 @@
+#calculadora de imc
+Nome = 'Yuri'
+altura = 1.76
+peso = 110
+print ('IMC é o Peso divido pela altura elevada ao quadrado.')
+print (Nome, 'seu IMC é de:')
+print ((peso / (altura ** 2)))
