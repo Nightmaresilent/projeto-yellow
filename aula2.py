1694377529ee3317103717ecc1a7366ae7193d25
Nightmaresilent <yurianticonishi@gmail.com>
2026-03-23 02:13:02 -0300

    aula de python

---
+++ aula2.py
@@ -0,0 +1,13 @@
+print(10==10)
+print(11==10)
+print(type(1.1))
+print(type(10==10))
+print (type(1+1))
+print (type(1-1))
+print (type(1*2))
+print (type(4/2))
+aqua = "água"
+print (aqua)
+darkness = "escuridão" 
+megumin = "anão"
+print ("uma deusa inutil:", aqua, "uma paladino depravada:", darkness, ", uma maga suicida:", megumin)
---
+++ aula3.py
@@ -0,0 +1,11 @@
+nome = 'Yuri'
+Sobrenome = 'Antico'
+idade = 24
+ano_de_nascimento = 2001
+altura = 1.76
+print('nome:', nome)
+print('sobrenome:', Sobrenome)
+print('idade:', idade, 'anos')
+print('ano de nascimento:', ano_de_nascimento)
+print('é maior de idade?', idade >= 18)
+print('altura:', altura)
