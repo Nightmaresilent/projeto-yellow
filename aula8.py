9565d7ee48d4e4ff2d315af8a380fe765a02370d
Nightmaresilent <yurianticonishi@gmail.com>
2026-03-25 10:05:35 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,6 @@
+primeiro_valor = input ('Digite um valor: ')
+segundo_valor = input ('Digite outro valor: ')
+if primeiro_valor > segundo_valor:
+    print (f'{primeiro_valor}, o primeiro valor é maior do que segundo valor {segundo_valor}')
+else:
+    print (f'{segundo_valor} o segundo valor é maior do que o primeiro valor {primeiro_valor}')
