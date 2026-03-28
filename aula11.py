5c025d771874d5e5874e551eed5386d183c22a96
Nightmaresilent <yurianticonishi@gmail.com>
2026-03-28 00:04:39 -0300

    Create aula11.py

---
+++ aula11.py
@@ -0,0 +1,10 @@
+Convidados = ['João', 'Lucas', 'Leticia', 'Joana', 'Ana', 'Maria', 'Marcelo', 'Leandro', 'Sarah ']
+# if 'João' in Convidados:
+#     print ('Sim, João está convidado')
+# else:
+#     print ('Não foi convidado')
+Convidado = input ('qual seu nome?: ')
+if Convidado in Convidados:
+    print('Você está na lsita de convidados')
+else:
+    print('Você não foi convidado')
